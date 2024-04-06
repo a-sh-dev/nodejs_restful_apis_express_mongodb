69a878fafb99d9beb4342631f149c6492adf8800
a-sh-dev <iam.angie@ymail.com>
2024-04-06 17:57:15 +1100

    Initialise server on 8080

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+
+const app = express()
+
+// Authentication
+app.use(
+  cors({
+    credentials: true,
+  }),
+)
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(8080, () => {
+  console.log(`🚀 Server is running on http://localhost:8080/`)
+})
